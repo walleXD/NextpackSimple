0a1d6f765590affa4b4521ee39f510c38472b439
Walee Ahmed <walee.tausif@gmail.com>
2017-08-08 02:34:49 -0400

    refactored entry point

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ import { apiRouter } from './server/router'
 import conf from './client/next.config'
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, dir: './client', conf})
+const app = next({ dev, dir: './client', conf })
 const handle = app.getRequestHandler()
 
-app
-  .prepare()
-  .then(() => {
-    const server = express()
+const start = async () => {
+  await app.prepare()
+  const server = express()
 
-    server.use('/api', apiRouter)
+  server.use('/api', apiRouter)
 
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    })
-
-    server.listen(3000, (err) => {
-      if (err) throw err
-      console.log('> Ready on http://localhost:3000')
-    })
+  server.get('*', (req, res) => {
+    return handle(req, res)
   })
+
+  server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+}
+
+start()
